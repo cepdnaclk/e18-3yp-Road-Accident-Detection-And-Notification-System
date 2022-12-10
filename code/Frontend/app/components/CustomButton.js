974e9a88f79d0a997f757daef97dea2052083bd0
e18264 <e18264@eng.pdn.ac.lk>
2022-12-11 00:38:15 +0530

    done welcome screen

---
+++ code/Frontend/app/components/CustomButton.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+function CustomButton(props) {
+    return (
+        <TouchableOpacity 
+            onPress={() => props.onPress?.()}
+            style={{
+                height: 42, 
+                width: props.width, 
+                marginTop: 20 }}>
+                
+            <LinearGradient
+                style={styles.container}
+                colors={[ props.primary, props.secondary ]}
+                start={{x: 0.5, y: 0}}
+                end={{x: 0.5, y: 1}}>
+                
+                <Text style={{
+                    color: props.color, 
+                    fontFamily: props.font, 
+                    fontSize: 15}}> 
+
+                    {props.title}
+                </Text>
+            </LinearGradient>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 50,
+    }
+})
+
+export default CustomButton;
--- code/Frontend/app/screens/LoginScreen.js
+++ code/Frontend/app/screens/LoginScreen.js
@@ -1,12 +1,59 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { ImageBackground, Image, StyleSheet, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 
-function LoginScreen(props) {
+import CustomButton from '../components/CustomButton';
+
+const LoginScreen = ({navigation}) => {
+    const [fontsLoaded] = useFonts({
+        'Poppins': require('../assets/fonts/Poppins-Medium.ttf'),
+    });
+
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    }
+
     return (
-        <View>
-            <Text>This is Login Screen</Text>
+        <View style={styles.container}>
+            <ImageBackground source={require('../assets/img/Background.png')} style={styles.image}>
+                <View style={styles.welcomeLogo}>
+                    <Image source={require('../assets/img/LogoLogin.png')} />
+                </View>
+
+                {/* <View style={styles.buttons}>
+                    <CustomButton width='45%' font='Poppins' primary='#5037A9' secondary='#48319D' color='#FFFFFF' title='Sign Up' onPress={() => navigation.navigate('SignUp')}/>
+                    <CustomButton width='45%' font='Poppins' primary='#C7C0E3' secondary='#AEA6CC' color='#5037A9' title='Login' onPress={() => navigation.navigate('Login')}/>
+                </View> */}
+                <StatusBar style='light'/>
+            </ImageBackground>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+    },
+    image: {
+        flex: 1,
+        resizeMode: 'cover',
+        justifyContent: 'center',
+    },
+    welcomeLogo: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        paddingTop: '25%',
+    },
+    buttons: {
+        flex: 2,
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        paddingBottom: '25%',
+    },
+})
 
 export default LoginScreen;
