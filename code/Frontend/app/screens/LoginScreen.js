51ae45e3416a4d84be132b18bfed875463981e6f
e18264 <e18264@eng.pdn.ac.lk>
2022-12-11 01:13:31 +0530

    start a custom input component

--- code/Frontend/app/screens/LoginScreen.js
+++ code/Frontend/app/screens/LoginScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, Image, StyleSheet, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { ImageBackground, Image, StyleSheet, View, StatusBar, TouchableOpacity } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { useFonts } from 'expo-font';
 
@@ -19,14 +19,19 @@ const LoginScreen = ({navigation}) => {
         <View style={styles.container}>
             <ImageBackground source={require('../assets/img/Background.png')} style={styles.image}>
                 <View style={styles.welcomeLogo}>
+                    <TouchableOpacity 
+                        onPress={() => navigation.navigate('Welcome')}
+                        style={styles.back}>
+
+                        <Image source={require('../assets/icons/back.png')} />
+                    </TouchableOpacity>
                     <Image source={require('../assets/img/LogoLogin.png')} />
                 </View>
 
-                {/* <View style={styles.buttons}>
-                    <CustomButton width='45%' font='Poppins' primary='#5037A9' secondary='#48319D' color='#FFFFFF' title='Sign Up' onPress={() => navigation.navigate('SignUp')}/>
-                    <CustomButton width='45%' font='Poppins' primary='#C7C0E3' secondary='#AEA6CC' color='#5037A9' title='Login' onPress={() => navigation.navigate('Login')}/>
-                </View> */}
-                <StatusBar style='light'/>
+                <View style={styles.bottom}>
+                    <CustomButton width='90%' font='Poppins' primary='#5037A9' secondary='#48319D' color='#FFFFFF' title='Login' />
+                </View>
+                <ExpoStatusBar style='light'/>
             </ImageBackground>
         </View>
     );
@@ -48,11 +53,16 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-start',
         paddingTop: '25%',
     },
-    buttons: {
+    bottom: {
         flex: 2,
         alignItems: 'center',
         justifyContent: 'flex-end',
         paddingBottom: '25%',
+    },
+    back: {
+        position: 'absolute',
+        top: StatusBar.currentHeight + 15,
+        right: 20,
     },
 })
 
