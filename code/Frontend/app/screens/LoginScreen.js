636b911f6ab96a3041b94dab79dc19f20751269b
e18264 <e18264@eng.pdn.ac.lk>
2022-12-13 12:58:37 +0530

    edit login screen

--- code/Frontend/app/screens/LoginScreen.js
+++ code/Frontend/app/screens/LoginScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import { ImageBackground, Image, StyleSheet, View, StatusBar, TouchableOpacity } from 'react-native';
+import { ImageBackground, Image, StyleSheet, View, StatusBar, TouchableOpacity, ScrollView, useWindowDimensions } from 'react-native';
 // import AppLoading from 'expo-app-loading';
 import { useFonts } from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
@@ -9,6 +9,10 @@ import CustomButton from '../components/CustomButton';
 import CustomInput from '../components/CustomInput';
 
 const LoginScreen = ({navigation}) => {
+
+    const size = useWindowDimensions();
+    const height = size.height + StatusBar.currentHeight + 13;
+
     const [fontsLoaded] = useFonts({
         'Poppins': require('../assets/fonts/Poppins-Medium.ttf'),
     });
@@ -18,56 +22,59 @@ const LoginScreen = ({navigation}) => {
     }
 
     return (
-        <View style={styles.container}>
-            <ImageBackground source={require('../assets/img/Background.png')} style={styles.image}>
-                <View style={styles.welcomeLogo}>
-                    <TouchableOpacity 
-                        onPress={() => navigation.navigate('Welcome')}
-                        style={styles.back}>
+        <ScrollView>
+            <View style={[styles.container, {height: height}]}>
+                <ImageBackground source={require('../assets/img/Background.png')} style={styles.image}>
+                    <View style={styles.welcomeLogo}>
+                        <TouchableOpacity 
+                            onPress={() => navigation.navigate('Welcome')}
+                            style={styles.back}>
 
-                        <Ionicons name="md-chevron-back" size={35} color="#B5B5B5" />
-                        {/* <Ionicons name="md-arrow-back-outline" size={30} color="#B5B5B5" /> */}
-                    </TouchableOpacity>
-                    <Image source={require('../assets/img/LogoLogin.png')} />
-                </View>
-                
-                <View style={styles.bottom}>
-                    <CustomInput 
-                        width='90%' 
-                        font='Poppins' 
-                        iconName='account-outline'
-                        iconSize={20} 
-                        placeholder='username' 
-                        label='username'
-                        // error='This is an error message'
-                         />
-                    <CustomInput 
-                        width='90%' 
-                        font='Poppins' 
-                        iconName='lock-outline'
-                        iconSize={18}
-                        placeholder='password' 
-                        label='password'
-                        // error='This is an error message'
-                         />
+                            <Ionicons name="md-chevron-back" size={35} color="#B5B5B5" />
+                        </TouchableOpacity>
+                        <Image source={require('../assets/img/LogoLogin.png')} />
+                    </View>
+                    
+                    <View style={styles.middle}>
+                        <CustomInput 
+                            width='90%' 
+                            font='Poppins' 
+                            iconName='account-outline'
+                            iconSize={20} 
+                            placeholder='username' 
+                            label='username'
+                            // error='This is an error message'
+                            />
+                        <CustomInput 
+                            width='90%' 
+                            font='Poppins' 
+                            iconName='lock-outline'
+                            iconSize={18}
+                            placeholder='password' 
+                            label='password'
+                            // error='This is an error message'
+                            />
 
-                    <CustomButton 
-                        width='90%' 
-                        font='Poppins' 
-                        primary='#5037A9' 
-                        secondary='#48319D' 
-                        color='#FFFFFF' 
-                        title='Login' />
-                </View>
-                <ExpoStatusBar style='light'/>
-            </ImageBackground>
-        </View>
+                    </View>
+
+                    <View style={styles.bottom}>
+                        <CustomButton 
+                            width='90%' 
+                            font='Poppins' 
+                            primary='#5037A9' 
+                            secondary='#48319D' 
+                            color='#FFFFFF' 
+                            title='Login' />
+                    </View>
+                    <ExpoStatusBar style='light'/>
+                </ImageBackground>
+            </View>
+        </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         flexDirection: 'column',
     },
     image: {
@@ -81,11 +88,15 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-start',
         paddingTop: '25%',
     },
+    middle: {
+        flex: 3,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
     bottom: {
         flex: 2,
         alignItems: 'center',
-        justifyContent: 'flex-end',
-        paddingBottom: '25%',
+        justifyContent: 'center',
     },
     back: {
         position: 'absolute',
