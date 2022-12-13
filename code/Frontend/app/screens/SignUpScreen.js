b17c4db67b6fbe5853115c51ef640bee3c800b57
e18264 <e18264@eng.pdn.ac.lk>
2022-12-14 01:06:53 +0530

    almost done sign up

--- code/Frontend/app/screens/SignUpScreen.js
+++ code/Frontend/app/screens/SignUpScreen.js
@@ -1,6 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import { ImageBackground, Image, StyleSheet, View, StatusBar, TouchableOpacity, ScrollView, useWindowDimensions, Text } from 'react-native';
+import { 
+    ImageBackground, 
+    Image, 
+    StyleSheet, 
+    View, 
+    StatusBar, 
+    TouchableOpacity, 
+    ScrollView,
+    useWindowDimensions,
+    Text, 
+} from 'react-native';
 // import AppLoading from 'expo-app-loading';
 import { useFonts } from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
@@ -81,13 +91,14 @@ const renderItem = (state) => {
 
 const SignUpScreen = ({navigation}) => {
 
-    const [state, setState] = useState(1);
-
     const size = useWindowDimensions();
     const height = size.height + StatusBar.currentHeight + 13;
 
+    const [state, setState] = useState(1);
+
     const [fontsLoaded] = useFonts({
         'Poppins': require('../assets/fonts/Poppins-Medium.ttf'),
+        'BebasNeue': require('../assets/fonts/BebasNeue-Regular.ttf'),
     });
 
     if (!fontsLoaded) {
@@ -96,7 +107,7 @@ const SignUpScreen = ({navigation}) => {
 
     return (
         <ScrollView>
-            <View style={styles.container}>
+            <View style={[styles.container, {height: height}]}>
                 <ImageBackground source={require('../assets/img/Background.png')} style={styles.image}>
                     <View style={styles.welcomeLogo}>
                         <TouchableOpacity 
@@ -111,105 +122,116 @@ const SignUpScreen = ({navigation}) => {
                     <View style={styles.selection}>
                         <TouchableOpacity 
                             style={styles.btnSelection}
-                            onPress= {() => {setState(0)}}
-                            >
-                            <Text style={{color: '#FFFFFF', fontSize: 15, backgroundColor: '#000', padding: 5, width: 100}}>Ambulance</Text>
+                            onPress= {() => {setState(0)}}>
+
+                            <View style={[styles.buttons, state === 0 && styles.buttonsActive]}>
+                                <Image source={require('../assets/img/ambulance.png')} style={styles.btnIcon}/>
+                                {state === 0 && (<Text style={[styles.txtName, {fontFamily: 'BebasNeue'}]}>AMBULANCE</Text>)}
+                            </View>
                         </TouchableOpacity>
                         <TouchableOpacity 
                             style={styles.btnSelection}
-                            onPress= {() => {setState(1)}}
-                            >
-                            <Text style={{color: '#FFFFFF', fontSize: 15, backgroundColor: '#000', padding: 5, width: 100}}>Driver</Text>
+                            onPress= {() => {setState(1)}}>
+                                
+                            <View style={[styles.buttons, state === 1 && styles.buttonsActive]}>
+                                <Image source={require('../assets/img/driver.png')} style={styles.btnIcon}/>
+                                {state === 1 && (<Text style={[styles.txtName, {fontFamily: 'BebasNeue'}]}>DRIVER</Text>)}
+                            </View>
                         </TouchableOpacity>
                         <TouchableOpacity 
                             style={styles.btnSelection}
-                            onPress= {() => {setState(2)}}
-                            >
-                            <Text style={{color: '#FFFFFF', fontSize: 15, backgroundColor: '#000', padding: 5, width: 100}}>Emergency</Text>
+                            onPress= {() => {setState(2)}}>
+
+                            <View style={[styles.buttons, state === 2 && styles.buttonsActive, {paddingLeft: '15%'}]}>
+                                <Image source={require('../assets/img/emergency-contact.png')} style={styles.btnIcon}/>
+                                {state === 2 && (<Text style={[styles.txtName, {fontFamily: 'BebasNeue'}]}>EMERGENCY</Text>)}
+                            </View>
                         </TouchableOpacity>
                     </View>
-                    
-                    <View style={styles.middle}>
-                        <CustomInput 
-                            width='90%' 
-                            font='Poppins' 
-                            iconName='account-outline'
-                            iconSize={20} 
-                            placeholder='first name' 
-                            label='firstname'
-                            // error='This is an error message'
-                            />
-                        <CustomInput 
-                            width='90%' 
-                            font='Poppins' 
-                            iconName='account-outline'
-                            iconSize={20} 
-                            placeholder='last name' 
-                            label='lastname'
-                            // error='This is an error message'
-                            />
-                        <CustomInput 
-                            width='90%' 
-                            font='Poppins' 
-                            iconName='email-outline'
-                            iconSize={20} 
-                            placeholder='email' 
-                            label='email'
-                            // error='This is an error message'
-                            />
-                        <CustomInput 
-                            width='90%' 
-                            font='Poppins' 
-                            iconName='card-account-mail-outline'
-                            iconSize={16} 
-                            placeholder='NIC' 
-                            label='nic'
-                            // error='This is an error message'
-                            />
-                        <CustomInput 
-                            width='90%' 
-                            font='Poppins' 
-                            iconName='cellphone'
-                            iconSize={18} 
-                            placeholder='telephone number' 
-                            label='telephonenumber'
-                            // error='This is an error message'
-                            />
 
-                        {renderItem(state)}
-                        {/* Driver Ambulance Emergency */}
+                    <ScrollView>       
+                        <View style={styles.middle}>
+                            <CustomInput 
+                                width='90%' 
+                                font='Poppins' 
+                                iconName='account-outline'
+                                iconSize={20} 
+                                placeholder='first name' 
+                                label='firstname'
+                                // error='This is an error message'
+                                />
+                            <CustomInput 
+                                width='90%' 
+                                font='Poppins' 
+                                iconName='account-outline'
+                                iconSize={20} 
+                                placeholder='last name' 
+                                label='lastname'
+                                // error='This is an error message'
+                                />
+                            <CustomInput 
+                                width='90%' 
+                                font='Poppins' 
+                                iconName='email-outline'
+                                iconSize={20} 
+                                placeholder='email' 
+                                label='email'
+                                // error='This is an error message'
+                                />
+                            <CustomInput 
+                                width='90%' 
+                                font='Poppins' 
+                                iconName='card-account-mail-outline'
+                                iconSize={16} 
+                                placeholder='NIC' 
+                                label='nic'
+                                // error='This is an error message'
+                                />
+                            <CustomInput 
+                                width='90%' 
+                                font='Poppins' 
+                                iconName='cellphone'
+                                iconSize={18} 
+                                placeholder='telephone number' 
+                                label='telephonenumber'
+                                // error='This is an error message'
+                                />
 
-                        <CustomInput 
-                            width='90%' 
-                            font='Poppins' 
-                            iconName='lock-outline'
-                            iconSize={18} 
-                            placeholder='password' 
-                            label='password'
-                            // error='This is an error message'
-                            />
-                        <CustomInput 
-                            width='90%' 
-                            font='Poppins' 
-                            iconName='lock-outline'
-                            iconSize={18} 
-                            placeholder='confirm password' 
-                            label='confirmpassword'
-                            // error='This is an error message'
-                            />
+                            {renderItem(state)}
+                            {/* Driver Ambulance Emergency */}
 
-                    </View>
+                            <CustomInput 
+                                width='90%' 
+                                font='Poppins' 
+                                iconName='lock-outline'
+                                iconSize={18} 
+                                placeholder='password' 
+                                label='password'
+                                // error='This is an error message'
+                                />
+                            <CustomInput 
+                                width='90%' 
+                                font='Poppins' 
+                                iconName='lock-outline'
+                                iconSize={18} 
+                                placeholder='confirm password' 
+                                label='confirmpassword'
+                                // error='This is an error message'
+                                />
 
-                    <View style={styles.bottom}>
-                        <CustomButton 
-                            width='90%' 
-                            font='Poppins' 
-                            primary='#5037A9' 
-                            secondary='#48319D' 
-                            color='#FFFFFF' 
-                            title='Sign Up' />
-                    </View>
-                    <ExpoStatusBar style='light'/>
+                        </View>
+
+                        <View style={styles.bottom}>
+                            <CustomButton 
+                                width='90%' 
+                                font='Poppins' 
+                                primary='#5037A9' 
+                                secondary='#48319D' 
+                                color='#FFFFFF' 
+                                title='Sign Up' />
+                        </View>
+                        <ExpoStatusBar style='light'/>
+                    </ScrollView>
                 </ImageBackground>
             </View>
         </ScrollView>
@@ -218,6 +240,8 @@ const SignUpScreen = ({navigation}) => {
 
 const styles = StyleSheet.create({
     container: {
+        // flex: 1,
+        height: '100%',
         flexDirection: 'column',
     },
     image: {
@@ -226,16 +250,22 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     welcomeLogo: {
-        flex: 1,
+        // flex: 1,
+        height: '20%',
         alignItems: 'flex-start',
         justifyContent: 'flex-start',
         paddingTop: StatusBar.currentHeight + 10,
         paddingLeft: '4%',
+        // backgroundColor: '#5037A9'
     },
     selection: {
-        flex: 1,
-        justifyContent: 'center',
-        flexDirection: 'row'
+        // flex: 1,
+        height: '20%',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        flexDirection: 'row',
+        paddingHorizontal: '10%',
+        // backgroundColor: '#5037A9',
     },
     middle: {
         flex: 3,
@@ -262,6 +292,58 @@ const styles = StyleSheet.create({
     btnSelection: {
         padding: 10,
     },
+    buttons: {
+        height: 68,
+        width: 68,
+        backgroundColor: 'rgba(42, 46, 83, 0.9)',
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+        shadowColor: "#1c1b21",
+        shadowOffset: {
+            width: 0,
+            height: 6,
+        },
+        shadowOpacity:  0.9,
+        shadowRadius: 5.62,
+        elevation: 8
+    },
+    btnIcon: {
+        width: 43, 
+        height: 43,
+    },
+    buttonsActive: {
+        position: 'relative',
+        height: 82,
+        width: 82,
+        backgroundColor: 'rgba(42, 46, 83, 0.9)',
+        borderRadius: 50,
+        borderWidth: 1,
+        borderColor: 'rgba(1, 163, 235, 0.5)',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+        shadowColor: "#1c1b21",
+        shadowOffset: {
+            width: 0,
+            height: 6,
+        },
+        shadowOpacity:  0.9,
+        shadowRadius: 5.62,
+        elevation: 8
+    },
+    btnIconActive: {
+        width: 55, 
+        height: 55,
+    },
+    txtName: {
+        position: 'absolute',
+        color: '#FFFFFF',
+        fontSize: 12,
+        top: 85,
+        letterSpacing: 1.5,
+    }
 })
 
 export default SignUpScreen;
