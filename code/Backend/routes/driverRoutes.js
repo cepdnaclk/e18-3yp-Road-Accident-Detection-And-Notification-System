efc203f0c2438f3a13e2201324d04e9c4189d943
unknown <shehan2k@gmail.com>
2022-12-19 20:15:27 +0530

    Emergency Contact adding removing routes updated

--- code/Backend/routes/driverRoutes.js
+++ code/Backend/routes/driverRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const{registerDriver,loginDriver, getMe, removeMe, updateMe} = require('../controllers/driverController')
+const{registerDriver,loginDriver, getMe, removeMe, updateMe, addEmergency,removeEmergency} = require('../controllers/driverController')
 
 const {protect} = require('../middleware/driverAuthMiddleware')
 
@@ -9,5 +9,7 @@ router.post('/login',loginDriver);
 router.get('/me',protect,getMe);
 router.delete('/me',protect,removeMe);
 router.put('/me',protect,updateMe);
+router.put('/addemergency',protect,addEmergency);
+router.put('/removeemergency/:phoneNum',protect,removeEmergency);
 
 module.exports = router
