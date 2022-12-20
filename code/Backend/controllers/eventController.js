b12f9d1d0d0890d9a8c6d6bbe3182a525aecee8f
AkithaPathirana <e18375@eng.pdn.ac.lk>
2022-12-20 21:25:59 +0530

    issues solved

--- code/Backend/controllers/eventController.js
+++ code/Backend/controllers/eventController.js
@@ -1,140 +1,40 @@
-
 const User = require('../models/userModel');
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 const asyncHandler = require('express-async-handler');
 
-// @desc Register User
-// @route POST /api/users
-// @access Public
-const registerUser =asyncHandler(async(req,res) =>{
-    const{name,email,password} = req.body;
-
-    if(!name || !email || !password){
-        res.status(400)
-        throw new Error('Please add all fields')
-    }
-
-    //check if user exists
-    const userExists = await User.findOne({email})
-
-    if(userExists){
-        res.status(400)
-        throw new Error('User already Exists')
-    }
-
-    //Hash password
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password,salt)
-
-    //Create user
-    const user = await User.create({
-        name,
-        email,
-        password:hashedPassword
-    })
-
-    if(user){
-        res.status(201).json({
-            _id:user.id,
-            name:user.name,
-            email:user.email,
-            token:generateToken(user._id),
-        })
-    }else{
-        res.status(400)
-        throw new Error('Invalid user Data')
-    }
-})
-
-// @desc Authenticate a User
-// @route POST /api/users/login
-// @access Public
-const loginUser =asyncHandler( async(req,res) =>{
-    const {email,password} = req.body
-
-    const user = await User.findOne({email})
-
-    if(user &&(await bcrypt.compare(password,user.password))){
-        res.json({
-            _id:user.id,
-            name:user.name,
-            email:user.email,
-            token:generateToken(user._id),
-        })
-    } else{
-        res.status(400);
-        throw new Error('Invalid Credentials');
-    }
-})
-
-// @desc Get User data
-// @route GET /api/users/me
-// @access Public
-const getMe = asyncHandler( async(req,res) =>{
-   
-    const{_id,name,email} = await User.findById(req.user.id)
-
-    res.status(200).json({
-        id:_id,
-        name,
-        email,
-    })
-});
-
-//Generate JWT
-const generateToken = (id) =>{
-    return jwt.sign({id},process.env.JWT_SECRET,{
-        expiresIn:'30d',
-    })
-}
-
-module.exports = {
-    registerUser,
-    loginUser,
-    getMe
-}
-
-
 //added new
-// @desc Get Accident Location
+// @desc Get Acci
 // @route POST /api/users/location
 // @access Public
-const getLocation =asyncHandler(async(req,res) =>{
+const findAmbulance =asyncHandler(async(req,res) =>{
     const{longitude,latitude,device,activeStatus} = req.body;
 
     if(!longitude || !latitude || !device || activeStatus){
         res.status(400)
         throw new Error('Please add all fields')
     }
+    
+    try{
 
+        const latitude=req.body.latitude;
+        const longitude=req.body.longitude;
 
+        const find_ambulance=await Ambulance.aggragate([
+            {
+                $geoNear:{
+                    near:{type:"Point",coordinates:[parseFloat(longitude),parseFloat(latitude)]},
+                    key:"location",
+                    maxDistance:parseFloat(1000)*1609,
+                    distanceField:"dist.calculated",
+                    spherical:true
+                }
+            }
+        ]);
 
-    //Create location
-    const accident = await Location.create({
-        longitude,
-        latitude,
-        device,
-        activeStatus
-        
-    })
-
-    if(accident){
-        res.status(201).json({
-            longitude:accident.longitude,
-            latitude:accident.latitude,
-            device:accident.device,
-            activeStatus:accident.activeStatus,
-        })
-    }else{
-        res.status(400)
-        throw new Error('Invalid user Data')
+    } catch(error){
+        res.status(400).send({success:false,msg:error.message})
     }
+
 })
 
-module.exports = {
-    registerUser,
-    loginUser,
-    getMe,
-    getLocation
-}
